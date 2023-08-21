14af52c12ae11d13de98f632674ee03467501d41
Christian Iwata Nilsson <christian.nilsson@caspeco.se>
2023-08-21 11:25:35 +0200

    VatNumber and ICountry structure

---
+++ VatValidation/Countries/ICountry.cs
@@ -0,0 +1,10 @@
+namespace VatValidation.Countries;
+
+public interface ICountry
+{
+	bool Valid(VatNumber vat);
+	string CC { get; }
+	string FormatStripped(VatNumber vat);
+	string FormatNational(VatNumber vat);
+	string FormatVat(VatNumber vat);
+}
