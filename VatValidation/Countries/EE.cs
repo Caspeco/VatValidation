46ef8df3daec775f83461891e9ef51265a46317a
Christian Iwata Nilsson <christian.nilsson@caspeco.se>
2024-08-23 11:41:30 +0200

    Migrate EE testcases to XML comments

--- VatValidation/Countries/EE.cs
+++ VatValidation/Countries/EE.cs
@@ -1,5 +1,24 @@
+
 namespace VatValidation.Countries;
 
+/// <country>Estonia</country>
+/// <testcases>
+/// valid, in: 100931558, national: 100931558, vat: EE 100931558, stripped: 100931558, vatstripped: EE 100931558
+/// valid, in: 100 931 558, national: 100931558, vat: EE 100931558, stripped: 100931558, vatstripped: EE 100931558
+/// valid, in: 100594102, national: 100594102, vat: EE 100594102, stripped: 100594102, comment: matches both EE and NO
+/// invalid, in: 100 594 102x, strippedvalid, national: 100594102, vat: EE 100594102, stripped: 100594102
+/// invalid, in: 100594103, national: 100594103, vat: EE 100594103, stripped: 100594103, vatstripped: EE 100594103
+/// invalid, in: 999999999, national: 999999999, vat: EE 999999999, stripped: 999999999, vatstripped: EE 999999999, dontTryParse
+/// invalid, in: 999 999 999, national: 999 999 999, vat: EE 999 999 999, stripped: 999999999, vatstripped: EE 999999999, dontTryParse
+/// invalid, in: 999 999 99, national: 999 999 99, vat: EE 999 999 99, stripped: 99999999, vatstripped: EE 99999999
+/// invalid, in: 99999999, national: 99999999, vat: EE 99999999, stripped: 99999999, vatstripped: EE 99999999
+/// invalid, in: 099999999, national: 099999999, vat: EE 099999999, stripped: 099999999, vatstripped: EE 099999999
+/// valid, in: EE100931558, national: 100931558, vat: EE 100931558, stripped: 100931558, vatstripped: EE 100931558
+/// valid, in: EE 100 931 558, national: 100931558, vat: EE 100931558, stripped: 100931558, vatstripped: EE 100931558
+/// invalid, in: EE 999 999 999, national: EE 999 999 999, vat: EE EE 999 999 999, stripped: 999999999, vatstripped: EE 999999999
+/// invalid, in: EE999999999, national: EE999999999, vat: EE EE999999999, stripped: 999999999, vatstripped: EE 999999999
+/// valid, in: EE 100594102, national: 100594102, vat: EE 100594102, stripped: 100594102
+/// </testcases>
 public class EE : CountryBase
 {
 	private EE() { }
