19f5f72bd0676f09f28b4382df9ed6332c77c3d5
Christian Iwata Nilsson <christian.nilsson@caspeco.se>
2024-08-23 09:22:32 +0200

    Migrate NO testcases to XML comments

--- VatValidation/Countries/NO.cs
+++ VatValidation/Countries/NO.cs
@@ -1,5 +1,20 @@
+
 namespace VatValidation.Countries;
 
+/// <country>Norway</country>
+/// <testcases>
+/// valid, in: 977074010, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010
+/// valid, in: 977 074 010, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010
+/// invalid, in: 977 074 010x, strippedvalid, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010
+/// invalid, in: 977074011, national: 977074011, vat: NO 977074011, stripped: 977074011, vatstripped: NO 977074011
+/// invalid, in: 977 074 011, national: 977 074 011, vat: NO 977 074 011, stripped: 977074011, vatstripped: NO 977074011
+/// invalid, in: 977 074 01, national: 977 074 01, vat: , stripped: 97707401, vatstripped:
+/// invalid, in: 97707401, national: 97707401, vat: , stripped: 97707401, vatstripped:
+/// invalid, in: 777074016, national: 777074016, vat: , stripped: 777074016, vatstripped:
+/// valid, in: 977 074 010MVA, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010, vatstripped: NO 977 074 010
+/// valid, in: NO 977 074 010MVA, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010, vatstripped: NO 977 074 010
+/// valid, in: NO977074010, national: 977 074 010, vat: NO 977 074 010, stripped: 977074010, vatstripped: NO 977 074 010
+/// </testcases>
 public class NO : CountryBase
 {
 	private NO() { }
