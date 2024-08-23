a3d230ba28b2117c58f77dc9c2d59c55a32ef6ac
Christian Iwata Nilsson <christian.nilsson@caspeco.se>
2024-08-23 13:47:54 +0200

    Migrate PT testcases to XML comments

--- VatValidation/Countries/PT.cs
+++ VatValidation/Countries/PT.cs
@@ -1,5 +1,23 @@
+
 namespace VatValidation.Countries;
 
+/// <country>Portugal</country>
+/// <testcases>
+/// valid, in: 999999990, national: 999999990, stripped: 999999990, vat: PT 999999990, vatstripped: PT 999999990
+/// valid, in: 287024008, national: 287024008, stripped: 287024008, vat: PT 287024008, vatstripped: PT 287024008
+/// valid, in: 287 024 008, national: 287024008, stripped: 287024008, vat: PT 287024008, vatstripped: PT 287024008
+/// valid, in: 501442600, national: 501442600, stripped: 501442600, vat: PT 501442600, vatstripped: PT 501442600
+/// invalid, in: 999999999, national: 999999999, stripped: 999999999, vat: PT 999999999, vatstripped: PT 999999999, dontTryParse
+/// invalid, in: 999 999 999, national: 999 999 999, stripped: 999999999, vat: PT 999 999 999, vatstripped: PT 999999999, dontTryParse
+/// invalid, in: 999 999 99, national: 999 999 99, stripped: 99999999, vat: PT 999 999 99, vatstripped: PT 99999999
+/// invalid, in: 099999999, national: 099999999, stripped: 099999999, vat: PT 099999999, vatstripped: PT 099999999
+/// valid, in: PT 999999990, national: 999999990, stripped: 999999990, vat: PT 999999990, vatstripped: PT 999999990
+/// valid, in: PT 999 999 990, national: 999999990, stripped: 999999990, vat: PT 999999990, vatstripped: PT 999999990
+/// valid, in: PT999999990, national: 999999990, stripped: 999999990, vat: PT 999999990, vatstripped: PT 999999990
+/// valid, in: PT 287024008, national: 287024008, stripped: 287024008, vat: PT 287024008, vatstripped: PT 287024008
+/// valid, in: PT 501442600, national: 501442600, stripped: 501442600, vat: PT 501442600, vatstripped: PT 501442600
+/// invalid, in: PT 999 999 999, national: PT 999 999 999, stripped: 999999999, vat: PT PT 999 999 999, vatstripped: PT 999999999
+/// </testcases>
 public class PT : CountryBase
 {
 	private PT() { }
